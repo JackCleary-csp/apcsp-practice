38fe164186253f1e564d8ec3dc0881331ef4c66d
JackCleary-csp <jack.cleary27@student.tamdistrict.org>
2026-08-27 18:37:18 +0000

    initial commit

---
+++ 3-4/sequence_practice.py
@@ -0,0 +1,13 @@
+scores = [72, 86, 91, 68, 88]
+title = "weekly score report"
+print(scores[0])
+print(scores[2])
+print(scores[4])
+print(scores[0:])
+print(title[0:6])
+scores.append(93)
+print(title[0:6])
+print(title[13:])
+weekly = title[0:6]
+report = title[13:
+num_scores = weekly + ": " + str(scores)
