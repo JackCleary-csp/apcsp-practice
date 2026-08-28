09a1f9dcf9f186e811d9d07b26797c85772aad9b
JackCleary-csp <jack.cleary27@student.tamdistrict.org>
2026-08-27 22:11:13 -0700

    Implement weekly score report functionality
    Added a score list and title for a weekly report, printed specific scores and parts of the title, and appended a new score.

--- topic-3-4/sequence_practice.py
+++ topic-3-4/sequence_practice.py
@@ -1,1 +1,12 @@
-MR sf i have no idea how to work github and my stuff deleted and I wasn't here monday tuesday but i did the stuff in class but it dissapeared. is there any way I can come in during a smart period to make this up and you help me better learn github?
+scores = [72, 86, 91, 68, 88]
+title = "weekly score report"
+print(scores[0])
+print(scores[2])
+print(scores[4])
+print(scores[0:])
+print(title[0:6])
+scores.append(93)
+print(title[13:])
+weekly = title[0:6]
+report = title[13:
+num_scores = weekly + ": " + str(scores)
