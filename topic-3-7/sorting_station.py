370aa3db396febc3ebad33fe6ea12333facb8fd6
JackCleary-csp <jack.cleary27@student.tamdistrict.org>
2026-09-17 05:07:11 +0000

    sorting station practice

---
+++ topic-3-7/sorting_station.py
@@ -0,0 +1,26 @@
+label = input()
+
+shape = label[0:4]
+color = label[4:7]
+size = int(label[7:10])
+mass = int(label[10:14])
+condition = label[14]
+
+if condition == "D" or size > 50 or mass > 2000:
+    destination = "INSPECT"
+else:
+    if shape == "BALL":
+        if color == "RED" and size >10:
+            destination = "B"
+        else:
+            destination = "A"
+    else:
+        if shape == "CUBE":
+            if (color == "BLU" or color == "GRN") and size <= 10:
+                destination = "C"
+            else:
+                destination = "D"
+        else:
+            destination = "E"
+
+print(destination)
